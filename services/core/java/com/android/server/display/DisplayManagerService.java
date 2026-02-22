2a540274441315afbc01cc036fe7e97ad92520c0
CuriousNom <aryanbiswas7585@gmail.com>
2026-02-22 21:05:00 +0000

    Revert "base: Add support for LMOFreeform service"
    This reverts commit ca63e07a12e7b89d2a529d1442d338bae6ea9038.

--- services/core/java/com/android/server/display/DisplayManagerService.java
+++ services/core/java/com/android/server/display/DisplayManagerService.java
@@ -180,8 +180,6 @@ import com.android.server.utils.FoldSettingProvider;
 import com.android.server.wm.SurfaceAnimationThread;
 import com.android.server.wm.WindowManagerInternal;
 
-import com.libremobileos.freeform.ILMOFreeformDisplayCallback;
-
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -518,8 +516,6 @@ public final class DisplayManagerService extends SystemService {
     // If we would like to keep a particular eye on a package, we can set the package name.
     private final boolean mExtraDisplayEventLogging;
     private final String mExtraDisplayLoggingPackageName;
-
-    private LMOFreeformDisplayAdapter mFreeformDisplayAdapter;
 
     private final BroadcastReceiver mIdleModeReceiver = new BroadcastReceiver() {
         @Override
@@ -1974,7 +1970,6 @@ public final class DisplayManagerService extends SystemService {
             if (shouldRegisterNonEssentialDisplayAdaptersLocked()) {
                 registerOverlayDisplayAdapterLocked();
                 registerWifiDisplayAdapterLocked();
-                registerFreeformDisplayAdapterLocked();
             }
         }
     }
@@ -1993,13 +1988,6 @@ public final class DisplayManagerService extends SystemService {
                     mPersistentDataStore, mFlags);
             registerDisplayAdapterLocked(mWifiDisplayAdapter);
         }
-    }
-
-    private void registerFreeformDisplayAdapterLocked() {
-        mFreeformDisplayAdapter = new LMOFreeformDisplayAdapter(
-                mSyncRoot, mContext, mHandler, mDisplayDeviceRepo, mLogicalDisplayMapper,
-                mUiHandler, mFlags);
-        registerDisplayAdapterLocked(mFreeformDisplayAdapter);
     }
 
     private boolean shouldRegisterNonEssentialDisplayAdaptersLocked() {
@@ -5161,23 +5149,6 @@ public final class DisplayManagerService extends SystemService {
         @Override
         public void onPresentation(int displayId, boolean isShown) {
             mExternalDisplayPolicy.onPresentation(displayId, isShown);
-        }
-
-        public void createFreeformLocked(String name, ILMOFreeformDisplayCallback callback,
-                int width, int height, int densityDpi, boolean secure, boolean ownContentOnly,
-                boolean shouldShowSystemDecorations, Surface surface, float refreshRate,
-                long presentationDeadlineNanos) {
-            mFreeformDisplayAdapter.createFreeformLocked(name, callback, width, height, densityDpi,
-                    secure, ownContentOnly, shouldShowSystemDecorations, surface, refreshRate,
-                    presentationDeadlineNanos);
-        }
-
-        public void resizeFreeform(IBinder appToken, int width, int height, int densityDpi) {
-            mFreeformDisplayAdapter.resizeFreeform(appToken, width, height, densityDpi);
-        }
-
-        public void releaseFreeform(IBinder appToken) {
-            mFreeformDisplayAdapter.releaseFreeform(appToken);
         }
     }
 
