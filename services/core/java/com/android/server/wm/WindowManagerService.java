77d74eb6028f6a74398ce4221b1dcb0a91d17dc5
CuriousNom <aryanbiswas7585@gmail.com>
2026-02-22 21:05:00 +0000

    Revert "wm: Add API to listen for secure content in display"
    This reverts commit 6c422a8035229382889843bfeb0437611c6c8c79.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -790,9 +790,6 @@ public class WindowManagerService extends IWindowManager.Stub
     WindowManagerInternal.OnImeRequestedChangedListener mOnImeRequestedChangedListener;
     @Nullable ImeTargetChangeListener mImeTargetChangeListener;
 
-    private ArraySet<WindowManagerInternal.DisplaySecureContentListener>
-            mDisplaySecureContentListeners = new ArraySet<>();
-
     SettingsObserver mSettingsObserver;
     final EmbeddedWindowController mEmbeddedWindowController;
     final AnrController mAnrController;
@@ -5446,15 +5443,6 @@ public class WindowManagerService extends IWindowManager.Stub
         }
     }
 
-    void notifyDisplaySecureContentChange(int displayId, boolean hasSecureWindowOnScreen) {
-        synchronized (mGlobalLock) {
-            mDisplaySecureContentListeners.forEach((listener) -> {
-                listener.onDisplayHasSecureWindowOnScreenChanged(
-                        displayId, hasSecureWindowOnScreen);
-            });
-        }
-    }
-
     // -------------------------------------------------------------
     // Input Events and Focus Management
     // -------------------------------------------------------------
@@ -8860,20 +8848,6 @@ public class WindowManagerService extends IWindowManager.Stub
         public ScreenshotHardwareBuffer takeAssistScreenshot(Set<Integer> windowTypesToExclude) {
             // WMS.takeAssistScreenshot takes care of the locking.
             return WindowManagerService.this.takeAssistScreenshot(windowTypesToExclude);
-        }
-
-        @Override
-        public void registerDisplaySecureContentListener(DisplaySecureContentListener listener) {
-            synchronized (mGlobalLock) {
-                mDisplaySecureContentListeners.add(listener);
-            }
-        }
-
-        @Override
-        public void unregisterDisplaySecureContentListener(DisplaySecureContentListener listener) {
-            synchronized (mGlobalLock) {
-                mDisplaySecureContentListeners.remove(listener);
-            }
         }
     }
 
